bfa04f1a6123c8fad3e26c5cf8a0831ab649ca5c
Erica Nafziger and Ezra Sandzer-Bell <ezra@>
2016-10-31 12:00:19 -0700

    introduce remove checkbox id template - does not currently work to remove checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -15,17 +15,29 @@ $(document).ready(function(){
     var taskInput = $("input#task").val();
     var deadlineInput = $("input#deadline").val();
     var userTasks = new taskObject(taskInput, deadlineInput);
-    $(".tasks .form-group").append("<input type='checkbox' name='list' value='taskInput'> " + userTasks.taskInput + ", " + userTasks.deadlineInput + "<br>");
+    $(".tasks .form-group").append("<input type='checkbox' id='checkboxid' name='list' value='"+ taskInput + "'> " + userTasks.taskInput + ", " + userTasks.deadlineInput + "<br>");
 
-    // $(".clickable").last().click(function(){
-    //   $(".trip").show();
-    //   $(".trip h2").text(place.placeYear());
-    //   $(".place").text(place.placeInput);
-    //   $(".year").text(year.yearInput);
-    //   $(".landmark").text(landmark.landmarkInput);
-    //   $(".city").text(city.cityInput);
+    $(".tasks .form-group").click(function(){
 
-    // });
+      function deleteCheckBox(){
+
+        if (document.getElementById('checkboxid').checked){
+           $("#checkboxid").remove();
+        }
+      }
+  });
+console.log(document.getElementById('checkboxid').checked);
+
+      // $("input:checkbox[name=list]:checked").each(function(){
+      //   var checkedTask = document.getElementsByClassName('chk');
+      //   if (input.checked) {
+      //     box.parentNode.removeChild(box);
+      //     txt.parentNode.removeChild(txt);
+      //   }
+      //   console.log(checkedTask);
+      //   // $('.tasks .form-group').remove(checkedTask);
+      // });
+
     resetFields();
   });
 
